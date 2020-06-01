1b81927f3b61206aa82061de2c1e8edb068f821e
Kejian Shi <ks4765@nyu.edu>
2020-06-01 06:07:09 -0400

    Fix shit

--- vehicle_env.py
+++ vehicle_env.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 
-
-''' This is the code for road environment setup 
+''' This is the code for road environment setup with SI metric 
 '''
 import os 
 import sys 
@@ -13,25 +12,21 @@ __author__ = "Haoran Su, Kejian Shi"
 __version__ = "1.0.1"
 
 '''
-To do: 
+We are more likely to have a speed that does not differ too much. Now I only use random speed within a range proportional to the density setting. 
 
-acceleration 放到case里直接写
-
-format 这样写： '{} {}'. format(self.first, self. last) 
-
-def repr 
-
-value: random.randint(1,6) // inclusive 
-list = ['R','G','B']
-.choice(list) Can be list of classes 
-.choices(list, weights = [3,3,1] k = 4) //要4个 , 则3:3:1的形式rand 4个
-
-Velocity: an action following the resulting policy from DDPG neural network
-
-问题：
+Calculate method: Assume we have evenly spacing: we use a simple test calculation 2.5 +(2.5/density) * 0.447 = base + 2.5 (scalar / density in range 0.1 to 0.9) * 0.447 because conversion from mph to mps is 0.447 
 
 '''
 
+
+'''
+To do: 
+Density 肯定是长度算，因为固定2车道，宽是固定的，则
+
+两边的density 分开看就行！！
+'{} {}'. format(self.first, self. last) 
+
+'''
 class Timestamp: 
     '''
     10 - 30 seconds per instruction, but we need acclelration
@@ -40,89 +35,135 @@ class Timestamp:
     def get_time(self):
         pass 
 
-class Vehicle: 
-    def __init__(self, speed, length, max_acceler):
-        self.length = length 
-        self.speed = speed
-        self.max_acceler = max_acceler
-
-''' 
-metric: SI 
-
-'''
-class SmallV(Vehicle):
-    def __init__(self, speed):
+class SmallV():
+    def __init__(self):
         self.id = 0
-        self.length = random.randrange(4.10,4.74)
-        self. width = random.randrange(1.79,1.86)
-        self.max_acceler = random.randrange(2.0,3.0)
+        self.length = random.randrange(4.00,4.74)
+        self.width = random.randrange(1.79,1.86)
+        self.max_acceler = random.randrange(3.5,4.5)
+        self.speed = 10
+        self.center_position = None 
 
-class MediumV(Vehicle):
-    def __init__(self, speed):
+class MediumV():
+    def __init__(self):
         self.id = 1
         self.length = random.randrange(4.75,5.44)
         self.width = random.randrange(1.84,1.97)
-        self.max_acceler = random.randrange(2.5,6.5)
+        self.max_acceler = random.randrange(3.0,6.2)
+        self.speed = 10
+        self.center_position = None 
 
-class LargeV(Vehicle):
-    def __init__(self, speed):
+class LargeV():
+    def __init__(self):
         self.id = 2
         self.length = random.randrange(5.45,6.45)
         self.width = random.randrange(1.94,2.20)
-        self.max_acceler = random.randrange(2.0,4.0)
+        self.max_acceler = random.randrange(2.0,3.5)
+        self.speed = 10
+        self.center_position = None 
+
+def generate_num_margin_error(num, error_range):
+    return random.randrange(num - error_range , num + error_range)
+    
+def cal_spacing_and_density(env_density, curr_density, roadlen, a_vehicle):
+    '''get a safe spacing that ensures a "1.5 second rule" given nature of NYC 
+    '''
+    safe_spacing = a_vehicle.speed * 1.5 - 0.5 * (a_vehicle.max_acceler * 3) * 1.5^2  # braking decleration is about 3-4 times than acceleration
+
+    '''
+    todo: 见于堵的情况下倾向于squeeze， 而不dense更多选择会更分散， 则加上一个density 的影响因子. density 和 spacing 成反比则
+    '''
+    spacing = safe_spacing + random.randrange(-0.2,2.0) 
+
+    if spacing < 1:
+        spacing = 1
+    new_density = curr_density + (spacing + a_vehicle.length) / roadlen
+    return spacing , new_density 
 
 class Environment:  
     '''
     each car, index,length,physical range on the road, acceleration, env里分别有多少大中小车 (total不需单独出))  
-    需要 每辆车的center position, 关键还需要验证是不是合理（重叠，out_of_bounds），通过那个和长度算出 head 和 rear
-
-    最后的density 是按照面积除以总面积做？
-
-    这块要想想：
-    center position 其实可以放进Car. ASSIGN 是在Env里通过计算然后给他的
-
-    distribution 问题： 考不考虑 像density = 0.5, 是全部分散开0.5呢，还是一边全挤在一起， 怎么去tune这个条件
-
-    第二阶段: 旁边是什么车 (later)
+    需要 每辆车的center position, 关键还需要验证是不是合理（out_of_bounds），通过那个和长度算出 head 和 rear (stretch)
     '''
     def __init__(self, roadlen = 200, roadwid = 10, density = random.randrange(0.1,0.9)):
+        self.classes = (SmallV,MediumV,LargeV)
         self.num_smallV = 0
         self.num_mediumV = 0 
         self.num_largeV = 0
-        self.density = density
+        self.max_speed = 20
+        self.density = density 
+        self.curr_lane = 0 # 0 or 1 as 2-lane raod 
+        self.curr_lane_density = 0
         self.roadlen = roadlen
         self.roadwid = roadwid
-        self.classes = (SmallV,MediumV,LargeV)
-        self.vehicles = [] 
-    
-    def valid(self):
-        pass 
+        self.cur_cursor = roadlen # start position to fill vehicles 
+        self.env_status = [] # keep track of all vehicle information as array of [int:vehicle_index, int:type, int: lane, tuple:center_position, tuple<tuple>: physical_range_at_the_environment, int:speed] 
+       
+'''
+
+while True:
+...     try:
+...         x = int(input("Please enter a number: "))
+...         break
+...     except ValueError:
+...         print("Oops!  That was no valid number.  Try again...")
+
+'''
+
+    def position_valid(self, potential_vehicle):
+        
 
     def get_rand_vehicle(self):
-        a_vehicle = random.choice(self.classes)()
-        if a_vehicle.id == 0:
+        '''
+        # generate a vehicle with chance 1:3:1 being smallV,mediumV,largeV, respectively (reflect NYC traffic)
+        '''
+        a_vehicle = random.choices(self.classes, weights = [1,3,1], k = 1)() 
+        return a_vehicle
+
+    '''
+    @input road car density 取一个 range: 还是取0-1, 密度按 m/v 计算，让所有值落在0-1区间: normalize 
+    @return car index, position, (head_pos,rear_pos) 
+
+    '''
+    def is_valid(self):
+        return (self.curr_density < self.density) and (self.cur_cursor > 5) # otherwise cannot fit into any car by the 4m car length and 1m of minimum spacing
+
+    def generate_road_env(self):  
+       '''
+       1. while not less than minimum length requirement (with min of 1m spacing) get a car # The pre-checking is tough. 
+       2. generate_car and set a flag 
+       3. calculate spacing needed
+       3. 
+       3。check if rear will exceed the bound, or that it exceed density 
+       4. if not then put the car. with center position being half length + center of uni lane with small margin of error. 
+            update the env_status by append. and update the class variable 
+       5. if so, give it a small vehicle if possible. Use break and continue wisely 
+       6. switch another lane, repeat the process 
+       '''
+        # [int:vehicle_index, int:type, int: lane, tuple:center_position, tuple<tuple>: physical_range_at_the_environment (), int:speed] 
+
+       # ((self.cur_cursor - 5.74) > 0 ) # here we take a maximum small car with minimum spacing of 1m as a threshold 
+        while self.is_valid(): 
+            
+
+        index = 1 
+        while 
+
+        '''
+        肯定是，density 过了， 或次小的车已经放不下了
+
+ if a_vehicle.id == 0:
             self.num_smallV += 1 
         elif a_vehicle.id == 1:     
             self.num_mediumV += 1
         elif a_vehicle.id == 2:
             self.num_largeV += 1
 
-    '''
-    @input road car density 取一个 range: 还是取0-1, 密度按 m/v 计算，让所有值落在0-1区间: normalize 
-    @return car index, position, (head_pos,rear_pos) 
-    '''
-
-    def is_valid(self):
-        pass
-
-    def generate_road_env(self):  
+        ''''
         while self.is_valid():
             print(1)
-    '''
-    需要根据density 的设置，产生random生产的上限
 
-    强制车的间距： 可以evenly distributed 
-    '''
+
     def __repr__(self):
         pass 
 
@@ -134,4 +175,9 @@ if __name__ == "__main__":
     from random import choice
 classes = (RoomSpider, RoomSoldier, RoomDragon, ...)
 room = [[choice(classes)() for _ in range(2)] for __ in range(2)]
+
+   grid = [[1]*8 for i in range(8)]
+   for row in grid:
+       for column in row:
+           da
     '''
