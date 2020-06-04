15035a48bb13e1afe77744ef61512a562e068057
Kejian Shi <ks4765@nyu.edu>
2020-06-04 15:16:48 -0400

    fix

--- IDM.py
+++ IDM.py
@@ -4,13 +4,8 @@
 ''' This is the implementation for 1-D version of Intelligent Driver Model (IDM) 
     Class IDM() is a static class, model for 1-lane driver following behavior.  Metric: SI  
 '''
-import os 
 import sys 
-import random 
-
+import math 
 
 class IDM(object):
-    def __init__(self,v0,a,b, s0 = 2, T = 1.5 ) :
-        self.v0, self.delta, self.a, self.b, self.s0,  \
-            self.T = v0, DELTA, a, b, s0, T
-        
+    pass 
